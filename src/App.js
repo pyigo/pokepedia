aed102800c4d11cf1171a2f7cb5fce3e825ab17f
Paulette YIGO <paulette@Paulettes-MacBook-Air.local>
2022-01-25 11:40:54 -0500

    installed packages. Rendering Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// Import Components
+import "./App.css";
+// importing the component Nav, react is programmed to look for index.
+// if the the index is named anything than index ww have to include in in the path
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Pokepedia</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="#">Pokemon List</a>
+                        <a className="nav-link" href="#">Login</a>
+
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
